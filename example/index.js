2a537c275070f5cdebb003fe7786b6145f9d2bc9
Luigi Pinca <luigipinca@gmail.com>
2016-11-22 10:55:34 +0100

    [example] Fix the example

--- example/index.js
+++ example/index.js
@@ -1,13 +1,13 @@
 'use strict';
 
-var session = require('express-session')
-  , ShopifyToken = require('../')
-  , express = require('express')
-  , config = require('./config');
+const session = require('express-session');
+const express = require('express');
 
-var shopifyToken = new ShopifyToken(config);
+const ShopifyToken = require('..');
+const config = require('./config');
 
-var app = express();
+const shopifyToken = new ShopifyToken(config);
+const app = express();
 
 app.use(session({
   secret: 'eo3Athuo4Ang5gai',
@@ -15,20 +15,20 @@ app.use(session({
   resave: false
 }));
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   if (req.session.token) return res.send('Token ready to be used');
 
   //
   // Generate a random nonce.
   //
-  var nonce = shopifyToken.generateNonce();
+  const nonce = shopifyToken.generateNonce();
 
   //
   // Generate the authorization URL. For the sake of simplicity the shop name
   // is fixed here but it can, of course, be passed along with the request and
   // be different for each request.
   //
-  var uri = shopifyToken.generateAuthUrl(config.shop, undefined, nonce);
+  const uri = shopifyToken.generateAuthUrl(config.shop, undefined, nonce);
 
   //
   // Save the nonce in the session to verify it later.
@@ -37,8 +37,8 @@ app.get('/', function (req, res) {
   res.redirect(uri);
 });
 
-app.get('/callback', function (req, res) {
-  var state = req.query.state;
+app.get('/callback', (req, res) => {
+  const state = req.query.state;
 
   if (
       typeof state !== 'string'
@@ -51,20 +51,18 @@ app.get('/callback', function (req, res) {
   //
   // Exchange the authorization code for a permanent access token.
   //
-  shopifyToken.getAccessToken(req.query.shop, req.query.code, function (err, token) {
-    if (err) {
+  shopifyToken.getAccessToken(req.query.shop, req.query.code)
+    .then((token) => {
+      console.log(token);
+
+      req.session.token = token;
+      req.session.state = undefined;
+      res.redirect('/');
+    })
+    .catch((err) => {
       console.error(err.stack);
-      return res.status(500).send('Oops, something went wrong');
-    }
-
-    console.log(token);
-
-    req.session.token = token;
-    req.session.state = undefined;
-    res.redirect('/');
-  });
+      res.status(500).send('Oops, something went wrong');
+    });
 });
 
-app.listen(8080, function () {
-  console.log('Open http://localhost:8080 in your browser');
-});
+app.listen(8080, () => console.log('Open http://localhost:8080 in your browser'));
